ea44335e1fa72ae2dc541d5a495e2ef45cedddf6
Manish Verma <manish.verma217@gmail.com>
2023-07-28 10:20:44 +0530

    Initial Commit

---
+++ src/test/java/com/user/workspace/QCalc/QCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.workspace.QCalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
